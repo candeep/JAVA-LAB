80c4b5eb959af6793c89d7a32e80379447a91bce
R Sandeep <110334715+candeep@users.noreply.github.com>
2022-12-26 14:48:42 +0530

    Create week4-1.java

---
+++ week4-1.java
@@ -0,0 +1,24 @@
+class employee
+{
+	Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Name,Id and Designation: ");
+		String name=scan.next();
+		int id=scan.nextInt();
+		String desi=scan.next();
+	voi reademp()
+	{
+		
+	}
+}
+class salary
+{
+	void readsalary()
+	{
+	}
+	void calculatesalary()
+	{
+	}
+	void dispalyemp()
+	{
+	}
+}
